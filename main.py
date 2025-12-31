f66b63deae0b48dd4493991b4650aead94f9084e
JohnKos4 <johnk.media@gmail.com>
2025-12-31 17:10:54 +0100

    C: Added Player class and integrated into main loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -12,6 +13,8 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
@@ -19,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
         
